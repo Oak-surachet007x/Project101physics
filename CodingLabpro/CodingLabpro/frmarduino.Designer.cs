e4eab5f6908862ac3ea8b873af79e0f8e42d2919
Oak-surachet007x <117011637+Oak-surachet007x@users.noreply.github.com>
2024-11-11 22:38:35 +0700

    Craete Ui theme from video in frmarduino

---
+++ CodingLabpro/CodingLabpro/frmarduino.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CodingLabpro
+{
+    partial class frmarduino
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panelContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.Azure;
+            this.panelContainer.Controls.Add(this.panelTitleBar);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(1200, 600);
+            this.panelContainer.TabIndex = 0;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(56)))), ((int)(((byte)(255)))));
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(1200, 50);
+            this.panelTitleBar.TabIndex = 0;
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
+            // 
+            // frmarduino
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.panelContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmarduino";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmarduino";
+            this.Activated += new System.EventHandler(this.frmarduino_Activated);
+            this.Load += new System.EventHandler(this.frmarduino_Load);
+            this.ResizeEnd += new System.EventHandler(this.frmarduino_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.frmarduino_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmarduino_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmarduino_MouseDown);
+            this.panelContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
